ccd2b5d5d98f6ed0ec26eec6be5565f5eec8f42e
YourName <You@example.com>
2020-05-18 01:30:55 +0800

    1

---
+++ jump7.py
@@ -0,0 +1,6 @@
+Code/                                                                                               0000775 0011610 0011610 00000000000 13660201157 012533  5                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              Code/jump7.py                                                                                       0000664 0011610 0011610 00000000150 13660201157 014143  0                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              a=0
+while a<100:
+    a=a+1
+    if a % 7 ==0 or a % 10 ==7 or a // 10 ==7:
+        continue
+    print(a)
